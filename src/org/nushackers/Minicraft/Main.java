0249e05b8df16607cb89d50239701e78ea9b51e9
Shawn Tan <shawn@wtf.sg>
2011-12-23 18:13:59 +0800

    Android project created. Initial commit

---
+++ src/org/nushackers/Minicraft/Main.java
@@ -0,0 +1,26 @@
+package org.nushackers.Minicraft;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		View v = (View) findViewById(R.id.gamedisplay);
+		
+		Bitmap b = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+		Canvas cv = new Canvas(b);
+		
+		Drawable d = new Drawable();
+		//v.setBackgroundDrawable(b);
+		
+		
+	}
+	
+}
---
+++ src/org/nushackers/Minicraft/MinicraftActivity.java
@@ -0,0 +1,13 @@
+package org.nushackers.Minicraft;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MinicraftActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
